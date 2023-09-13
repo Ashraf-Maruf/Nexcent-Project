6b65c671998440f3ac170f2467c514f420bddb44
Ashraf-Maruf <s.m.a.ashraf41@gmail.comm>
2023-09-13 13:30:50 +0600

    Nexcent top button updata

--- src/Componet/TopButton.js
+++ src/Componet/TopButton.js
@@ -20,11 +20,14 @@ const TopButton = () => {
             behavior: 'smooth'
         })
     }
+    const boxShadow = {
+        boxShadow: '0px 3px 8px 0px #080a0b',
+    }
     return (
         <>
             {
                 backToTopButton && (
-                    <button className='fixed bottom-32 right-12 bg-green-900   rounded-md flex items-center justify-center animate-bounce w-12 h-12' onClick={scrollUP}><HiArrowSmallUp color='#080a0b' size={25} /></button>
+                    <button className='fixed z-30 sm1:hidden md:block bottom-72 right-12 bg-green-900   rounded-md flex items-center justify-center w-12 h-12 transition duration-700 ease-in-out  rotate-180 hover:rotate-0' onClick={scrollUP}><HiArrowSmallUp className='animate-bounce rounded-full' color='#080a0b' size={20} style={boxShadow} /></button>
                 )
             }
         </>
